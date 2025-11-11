8023f2063db108428c0f427b0fb0b7212ba12232
Bereket234 <bereketnigussie9@gmail.com>
2025-11-11 11:24:22 +0300

    add dotenv

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET || "dev_secret";
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "7d";
